429249a4868bfd87f64fcf094c4dffb012ef22f3
Marialafu <lafuentelhmaria@gmail.com>
2025-05-08 11:42:31 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import Button from './components/button/Button';
+import { useState } from 'react';
+import { USERS } from './constants/userInfo';
 
 const App = () => {
+  const [userNumber, setUserNumber] = useState(0);
+  const isFirstUser = userNumber <= 0;
+  const isLastUser = userNumber >= USERS.length - 1;
+
   return (
     <>
-      <Button />
-      <h1>React</h1>
+      <img src={USERS[userNumber].profileImage} alt='' />
+      <h2>Name:{USERS[userNumber].name}</h2>
+      <p>Username:{USERS[userNumber].username}</p>
+      <p>Email:{USERS[userNumber].email}</p>
+      <button
+        disabled={isFirstUser}
+        onClick={() => previousPicture(userNumber, setUserNumber)}
+      >
+        Previous
+      </button>
+      <button
+        disabled={isLastUser}
+        onClick={() => nextPicture(userNumber, setUserNumber)}
+      >
+        Next
+      </button>
     </>
   );
+};
+
+const nextPicture = (userNumber, setUserNumber) => {
+  setUserNumber(userNumber + 1);
+};
+
+const previousPicture = (userNumber, setUserNumber) => {
+  setUserNumber(userNumber - 1);
 };
 
 export default App;
